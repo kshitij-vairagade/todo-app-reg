9dd7eb41f8f67e1be5fb3c35198e320c0e7fd269
Kshitij <kv6124@gmail.com>
2021-12-07 02:29:22 +0530

    Final

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import "firebase/auth";
-import "firebase/firestore";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 import "firebase/storage"; 
 
 // TODO: Add SDKs for Firebase products that you want to use
